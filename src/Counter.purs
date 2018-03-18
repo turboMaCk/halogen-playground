26332e737b090c5d95c3148db3da97c24d4245fa
Marek Fajkus <marek.faj@gmail.com>
2018-03-18 19:35:07 +0100

    module separation

---
+++ src/Counter.purs
@@ -0,0 +1,64 @@
+module Counter (Query(..), MyAlgebra(..), ui) where
+
+import Prelude
+
+import Control.Monad.Free (Free, liftF)
+
+import Data.Maybe (Maybe(..))
+
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+
+
+type State = { count :: Int }
+
+
+data Query a = Increment a
+             | Decrement a
+
+
+data MyAlgebra a = Log String a
+type MyMonad = Free MyAlgebra
+
+
+output :: String -> MyMonad Unit
+output msg = liftF (Log msg unit)
+
+
+ui :: H.Component HH.HTML Query Unit Void MyMonad
+ui =
+  H.component
+    { initialState : const initialState
+    , render
+    , eval
+    , receiver : const Nothing
+    }
+  where
+
+  initialState :: State
+  initialState = { count: 0 }
+
+  render :: State -> H.ComponentHTML Query
+  render state =
+    HH.div_
+      [ HH.h1_
+          [ HH.text "Toggle Button" ]
+      , HH.button
+          [ HE.onClick (HE.input_ Increment) ]
+          [ HH.text "+" ]
+      , HH.text $ show state.count
+      , HH.button
+        [ HE.onClick (HE.input_ Decrement) ]
+        [ HH.text "-" ]
+      ]
+
+  eval :: Query ~> H.ComponentDSL State Query Void MyMonad
+  eval (Increment next) = do
+    H.modify (\state -> { count: state.count + 1 })
+    H.lift $ output "State was incremented"
+    pure next
+  eval (Decrement next) = do
+    H.modify (\state -> { count: state.count - 1 })
+    H.lift $ output "State was decremented"
+    pure next
--- src/Main.purs
+++ src/Main.purs
@@ -3,84 +3,30 @@ module Main where
 import Prelude
 
 import Control.Monad.Aff (Aff)
-import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Free (Free, liftF, foldFree)
+import Control.Monad.Free (foldFree)
 
-import Data.Maybe (Maybe(..))
-
+import Halogen.Aff (HalogenEffects, awaitBody, runHalogenAff)
+import Halogen.VDom.Driver (runUI)
 import Halogen as H
 import Halogen.Aff as HA
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.VDom.Driver (runUI)
+
+import Counter as Counter
 
 
-type State = { count :: Int }
-
-
-data Query a = Increment a
-             | Decrement a
-
-
-data MyAlgebra a = Log String a
-type MyMonad = Free MyAlgebra
-
-
-output :: String -> MyMonad Unit
-output msg = liftF (Log msg unit)
-
-
-ui :: H.Component HH.HTML Query Unit Void MyMonad
-ui =
-  H.component
-    { initialState : const initialState
-    , render
-    , eval
-    , receiver : const Nothing
-    }
+ui' :: forall eff. H.Component HH.HTML Counter.Query Unit Void (Aff (HalogenEffects (console :: CONSOLE | eff)))
+ui' = H.hoist (foldFree evalMyAlgebra) Counter.ui
   where
-
-  initialState :: State
-  initialState = { count: 0 }
-
-  render :: State -> H.ComponentHTML Query
-  render state =
-    HH.div_
-      [ HH.h1_
-          [ HH.text "Toggle Button" ]
-      , HH.button
-          [ HE.onClick (HE.input_ Increment) ]
-          [ HH.text "+" ]
-      , HH.text $ show state.count
-      , HH.button
-        [ HE.onClick (HE.input_ Decrement) ]
-        [ HH.text "-" ]
-      ]
-
-  eval :: Query ~> H.ComponentDSL State Query Void MyMonad
-  eval (Increment next) = do
-    H.modify (\state -> { count: state.count + 1 })
-    H.lift $ output "State was incremented"
-    pure next
-  eval (Decrement next) = do
-    H.modify (\state -> { count: state.count - 1 })
-    H.lift $ output "State was decremented"
-    pure next
-
-
-
-ui' :: forall eff. H.Component HH.HTML Query Unit Void (Aff (HA.HalogenEffects (console :: CONSOLE | eff)))
-ui' = H.hoist (foldFree evalMyAlgebra) ui
-  where
-  evalMyAlgebra :: MyAlgebra ~> Aff (HA.HalogenEffects (console :: CONSOLE | eff))
-  evalMyAlgebra (Log msg next) = do
+  evalMyAlgebra :: Counter.MyAlgebra ~> Aff (HA.HalogenEffects (console :: CONSOLE | eff))
+  evalMyAlgebra (Counter.Log msg next) = do
     log msg
     pure next
 
 
-main :: Eff (HA.HalogenEffects (console :: CONSOLE)) Unit
-main = HA.runHalogenAff do
-  body <- HA.awaitBody
+main :: Eff (HalogenEffects (console :: CONSOLE)) Unit
+main = runHalogenAff do
+  body <- awaitBody
   runUI ui' unit body
